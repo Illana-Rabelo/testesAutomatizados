09c9d18f59d5d9c054b0dadd199f69e432ff05f0
Illana-Rabelo <illana.rabelo@estudante.iftm.edu.br>
2026-04-13 20:14:25 -0300

    Update VeterinarioRepository.java

--- projeto-veterinario-springboot-h2-master/gerenciador-Veterinarios/src/main/java/org/iftm/gerenciadorveterinarios/repositories/VeterinarioRepository.java
+++ projeto-veterinario-springboot-h2-master/gerenciador-Veterinarios/src/main/java/org/iftm/gerenciadorveterinarios/repositories/VeterinarioRepository.java
@@ -1,12 +1,17 @@
 package org.iftm.gerenciadorveterinarios.repositories;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.iftm.gerenciadorveterinarios.entities.Veterinario;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 
-public interface VeterinarioRepository extends JpaRepository<Veterinario, Integer> {
-
-   public List<Veterinario> findByNomeContains(String nome);
+public interface VeterinarioRepository extends JpaRepository<Veterinario, Long> {
+    
+    // Para o Teste 1 
+    Optional<Veterinario> findByNome(String nome);
+    
+    // Para o Teste 4 
+    Optional<Veterinario> findByNomeIgnoreCase(String nome);
 }
