d4cf2e3717623e58fdbce254856cb10afce6d6f1
Illana-Rabelo <illana.rabelo@estudante.iftm.edu.br>
2026-04-13 20:34:25 -0300

    Add method for partial name search in VeterinarioRepository

--- projeto-veterinario-springboot-h2-master/gerenciador-Veterinarios/src/main/java/org/iftm/gerenciadorveterinarios/repositories/VeterinarioRepository.java
+++ projeto-veterinario-springboot-h2-master/gerenciador-Veterinarios/src/main/java/org/iftm/gerenciadorveterinarios/repositories/VeterinarioRepository.java
@@ -6,12 +6,13 @@ import java.util.Optional;
 import org.iftm.gerenciadorveterinarios.entities.Veterinario;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-
 public interface VeterinarioRepository extends JpaRepository<Veterinario, Long> {
-    
-    // Para o Teste 1 
-    Optional<Veterinario> findByNome(String nome);
-    
-    // Para o Teste 4 
-    Optional<Veterinario> findByNomeIgnoreCase(String nome);
+
+   // Para o Teste 1 (Busca exata do banco)
+   Optional<Veterinario> findByNome(String nome);
+
+   Optional<Veterinario> findByNomeIgnoreCase(String nome);
+
+   // Para o Teste 2 (Busca por parte do nome)
+   List<Veterinario> findByNomeContainingIgnoreCase(String trecho);
 }
