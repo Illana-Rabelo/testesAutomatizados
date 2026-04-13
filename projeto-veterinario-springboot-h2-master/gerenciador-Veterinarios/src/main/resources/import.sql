04016b3252880a56f5f0fab42abe55cb0aff8ea0
Illana-Rabelo <illana.rabelo@estudante.iftm.edu.br>
2026-04-13 20:38:22 -0300

    Update import.sql to modify veterinarian records
    Removed existing veterinarians and added new entries with birth dates.

--- projeto-veterinario-springboot-h2-master/gerenciador-Veterinarios/src/main/resources/import.sql
+++ projeto-veterinario-springboot-h2-master/gerenciador-Veterinarios/src/main/resources/import.sql
@@ -1,2 +1,8 @@
-INSERT INTO tb_veterinario (nome, email, especialidade, salario) VALUES('Conceição Evaristo', 'conceicao@gmail.com', 'pequenos', 3500.0);
-INSERT INTO tb_veterinario (nome, email, especialidade, salario) VALUES('Erica Queiroz Pinto', 'erica@gmail.com', 'grandes', 4500.0);
+DELETE FROM tb_veterinario;
+
+
+INSERT INTO tb_veterinario (nome, email, especialidade, salario, data_nascimento) 
+VALUES ('PEDRO', 'pedro@email.com', 'Clínica Geral', 6000.0, '1990-05-15T10:00:00Z');
+INSERT INTO tb_veterinario (nome, email, especialidade, salario, data_nascimento) VALUES ('Roberto Santos', 'roberto@email.com', 'Grandes Animais', 4500.0, '1985-10-20T10:00:00Z');
+INSERT INTO tb_veterinario (nome, email, especialidade, salario, data_nascimento) VALUES ('Conceição Evaristo', 'conceicao@email.com', 'Aves', 3500.0, '1995-03-10T10:00:00Z');
+INSERT INTO tb_veterinario (nome, email, especialidade, salario, data_nascimento) VALUES ('Erica Queiroz Pinto', 'erica@email.com', 'Cirurgia', 5500.0, '2000-01-01T10:00:00Z');
