d8c122af724a4f28f3a78bdeefb3d11e56cf9241
Illana-Rabelo <illana.rabelo@estudante.iftm.edu.br>
2026-05-18 20:53:29 -0300

    Add files via upload

---
+++ projeto-veterinario-springboot-h2-master/gerenciador-Veterinarios/src/main/java/org/iftm/gerenciadorveterinarios/repositories/AnimalRepository.java
@@ -0,0 +1,9 @@
+package org.iftm.gerenciadorveterinarios.repositories;
+
+import org.iftm.gerenciadorveterinarios.entities.Animal;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface AnimalRepository extends JpaRepository<Animal, Long> {
+}
